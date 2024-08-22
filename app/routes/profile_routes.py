c3cc7f3a3f1eaa705395417e5571ca81194d0744
Sulani Ishara <115069464+IsharaParanagamaGedara@users.noreply.github.com>
2024-08-23 00:47:15 +0530

    Add profile management routes for updating user profile

---
+++ app/routes/profile_routes.py
@@ -0,0 +1,42 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from app import db
+from app.models import User
+import datetime
+
+bp = Blueprint('profile', __name__, url_prefix='/profile')
+
+@bp.route('/', methods=['GET'])
+@jwt_required()
+def get_profile():
+    user_id = get_jwt_identity()['id']
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+    
+    user_data = {
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'email': user.email
+    }
+    
+    return jsonify(user_data), 200
+
+@bp.route('/update_profile', methods=['PUT'])
+@jwt_required()
+def update_profile():
+    data = request.get_json()
+    user_id = get_jwt_identity()['id']
+    user = User.query.get(user_id)
+    
+    if not user:
+        return jsonify({'message': 'User not found'}), 404
+
+    fields_to_update = {key: value for key, value in data.items() if key in ['first_name', 'last_name'] and value}
+    if fields_to_update:
+        for key, value in fields_to_update.items():
+            setattr(user, key, value)
+        user.update_date = datetime.datetime.utcnow()
+        db.session.commit()
+    
+    return jsonify({'message': 'Profile updated successfully'}), 200
